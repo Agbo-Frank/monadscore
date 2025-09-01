43550c56add88e79a824d246109f467f98b98ef6
Agbo Frank <agbofrank531@gmail.com>
2025-09-01 03:08:35 +0100

    Update API base URL in fetcher.js to correct production server address for accurate data fetching.

--- src/Api/fetcher.js
+++ src/Api/fetcher.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const baseURL = "https://scoreapi-production.up.railway.app";
+const baseURL = "https://scorerapi-production.up.railway.app";
 
 const axios = Axios.create({
   baseURL,
